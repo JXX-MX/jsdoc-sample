ba8dceeba2c09d9865cf0fdca5556d67abd45d65
jmpark-dev <fierce.unarmed_0l@icloud.com>
2022-11-07 18:56:20 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// @ts-check
+
+/**
+ * 들어온 인자를 그대로 리턴합니다.
+ * @param {number} id - This is Description
+ * @param {string} name - This is Description
+ * @return {string} id and name  - This is Description
+ */
+
+function foo(id, name) {
+  return `${id} and ${name}`;
+}
+
+/**
+ * 들어온 인자를 그대로 리턴합니다.
+ * @param {number} id - This is Description
+ * @param {string} name - This is Description
+ * @return {string} id and name  - This is Description
+ */
+
+function doo(id, name) {
+  return `${id} and ${name}`;
+}
+
+/** @type {string}*/
+const variable = "test";
+
+/**
+ * 할 일 아이템
+ * @typedef {Object} TodoItem
+ * @property {number} id - 할 일 id
+ * @property {string} content - 할 일 내용
+ */
+
+/**
+ * 할 일 목록
+ * @type {TodoItem[]}
+ */
+const todos = [
+  { id: 1, content: "HTML" },
+  { id: 2, content: "CSS" },
+  { id: 3, content: "Javascript" },
+];
+
+/**
+ * 들어온 인자를 그대로 리턴합니다.
+ * @param {number} id - This is Description
+ * @param {string} name - This is Description
+ * @return {string} id and name  - This is Description
+ */
+
+function doos(id, name) {
+  return `${id} and ${name}`;
+}
+
+/** @type {string}*/
+const variables = "test";
+
+/**
+ * 할 일 아이템
+ * @typedef {Object} TodoItems
+ * @property {number} id - 할 일 id
+ * @property {string} content - 할 일 내용
+ */
+
+/**
+ * 할 일 목록
+ * @type {TodoItems[]}
+ */
+const todoss = [
+  { id: 1, content: "HTML" },
+  { id: 2, content: "CSS" },
+  { id: 3, content: "Javascript" },
+];
